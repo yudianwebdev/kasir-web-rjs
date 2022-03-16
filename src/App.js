c85e92f503c0f4f002e3e8c946fafd3b047b1964
yudian nanda <yudiannanwebdev@gmail.com>
2022-03-16 14:19:56 +0700

    membuat menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style/global.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/auth/signin";
+import SignUp from "./pages/auth/signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<SignIn />}></Route>
+        <Route exact path="/about" element={<SignUp />}></Route>
+        {/* <Route exact path="/contact" element={<Contact />}></Route> */}
+      </Routes>
+    </Router>
   );
 }
 
